77ac82e4067c467320e0157d5a8688fa4811bebd
OwenLaRosa <owen.larosa@outlook.com>
2017-08-15 19:40:46 -0400

    feat: Add bio object, list contact info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,34 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  name: "Owen S. LaRosa",
+  role: "Mobile and Web Developer",
+  contact: {
+    email: "owen.larosa@gmail.com",
+    twitter: "@OwenLaRosa",
+    github: "OwenLaRosa",
+    blog: "blog.owenlarosa.com",
+    location: "Somerville, MA"
+  },
+  picture_url: "/images/owen.jpg",
+  welcome_message: "Hello, I'm a mobile developer and proud Udacian!",
+  skills: [
+    "Swift", "Java", "web development", "SQL", "curriculum planning"
+  ]
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var formattedContacts = [
+  HTMLemail.replace("%data%", bio.contact.email),
+  HTMLtwitter.replace("%data%", bio.contact.twitter),
+  HTMLgithub.replace("%data%", bio.contact.github),
+  HTMLblog.replace("%data%", bio.contact.blog),
+  HTMLlocation.replace("%data%", bio.contact.location)
+];
+
+formattedContacts.forEach(function(contactItem) {
+  $("#topContacts").append(contactItem);
+});
